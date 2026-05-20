5704f6c39b49016f49fe990bce1942a6fd50caf4
jalexw <jalexwhitman@gmail.com>
2026-05-20 16:15:08 -0300

    0.1.5 - global styling

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,6 +1,11 @@
 "use client";
 import { ErrorPage } from "@schemavaults/ui";
 
+import "@schemavaults/theme/globals.css";
+
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
+
 // Error boundaries must be Client Components
 
 export default function GlobalError({
@@ -12,8 +17,14 @@ export default function GlobalError({
 }) {
   return (
     // global-error must include html and body tags
-    <html lang="en" suppressHydrationWarning>
-      <body>
+    <html
+      lang="en"
+      className="w-full h-auto min-h-screen scroll-smooth"
+      suppressHydrationWarning
+    >
+      <body
+        className={`${inter.className} w-full flex flex-col grow min-h-screen h-full scroll-smooth`}
+      >
         <ErrorPage error={error} reset={reset} />
       </body>
     </html>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import type { Metadata } from "next";
 import "@schemavaults/theme/globals.css";
 import ClientProviders from "./client-providers";
 
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
+
 export const metadata: Metadata = {
   title: "SchemaVaults",
   description: "Schema-validated secure cloud data storage",
@@ -15,7 +18,9 @@ export default function RootLayout({ children }: PropsWithChildren) {
       className="w-full h-auto min-h-screen scroll-smooth"
       suppressHydrationWarning
     >
-      <body className="w-full h-auto min-h-screen scroll-smooth">
+      <body
+        className={`${inter.className} w-full flex flex-col grow min-h-screen h-full scroll-smooth`}
+      >
         <ClientProviders>{children}</ClientProviders>
       </body>
     </html>
