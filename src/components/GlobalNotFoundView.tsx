5ffdeac252444cbc2504bb7740cb4bca7519bad8
jalexw <jalexwhitman@gmail.com>
2026-05-20 18:43:07 -0300

    0.1.15 - link to homepage from global-not-fund.tsx

--- src/components/GlobalNotFoundView.tsx
+++ src/components/GlobalNotFoundView.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { ErrorPage } from "@schemavaults/ui";
+import { ErrorPage, Button } from "@schemavaults/ui";
 import type { ReactElement } from "react";
+import Link from "next/link";
+import { Home } from "lucide-react";
 
 export default function GlobalNotFoundView(): ReactElement {
   return (
@@ -11,6 +13,20 @@ export default function GlobalNotFoundView(): ReactElement {
       reset={(): void => {
         window.location.reload();
       }}
+      additionalButtons={[
+        <Link href="/">
+          <Button
+            variant={"secondary"}
+            className="flex flex-row flex-nowrap gap-2 items-center justify-start"
+            onClick={async () => {
+              window.location.href = "/";
+            }}
+          >
+            <Home className="h-4 w-4" />
+            Back to index page
+          </Button>
+        </Link>,
+      ]}
     />
   );
 }
